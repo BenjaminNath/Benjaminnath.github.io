db0837719443c45ca5650bc20064c5add12f1348
Ben <nathb3210@gmail.com>
2016-10-10 17:06:35 -0400

    fix issues on about page

--- script/myScript.js
+++ script/myScript.js
@@ -13,18 +13,31 @@ $('#calculator').hide().delay(1000).fadeIn(2000);
 
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    document.getElementById("toggleMenu").style.display = "none";
-    document.getElementById("portrait").style.left = "65%";
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("toggleMenu").style.display = "none";
+  document.getElementById("portrait").style.left = "65%";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
-    document.getElementById("toggleMenu").style.display = "initial";
-    document.getElementById("portrait").style.left = "50%";
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+  document.getElementById("toggleMenu").style.display = "initial";
+  document.getElementById("portrait").style.left = "50%";
 }
+
+//coffeeCups
+var options = {
+  useEasing : true,
+  useGrouping: true,
+  separator: ',',
+  decimal: '.',
+  prefix: '',
+  suffic:''
+};
+
+var coffee = new CountUp("coffeeCups", 0, 1528, 0, 5, options);
+coffee.start();
