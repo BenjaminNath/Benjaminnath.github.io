3d952f2fb587c1922bb7d56a7d085164a4a35e99
Ben <nathb3210@gmail.com>
2016-11-10 17:52:22 -0500

    add sticky imac image

--- script/myScript.js
+++ script/myScript.js
@@ -7,14 +7,12 @@ var code = new CountUp("codeLines", 0, 100000, 0, 5, options);
 var bugs = new CountUp("bugsFixed", 0, 130, 0, 5, options);
 
 // Changing the defaults
-window.sr = ScrollReveal({ reset: true });
+window.sr = ScrollReveal({ reset: false });
 
 // Customizing a reveal set
-sr.reveal('.title', { duration: 2000 });
 sr.reveal('#meText', {duration: 2000 });
 sr.reveal('.col', { duration: 2000 });
 sr.reveal('.skill_section', {duration: 2000});
-sr.reveal('.work',{duration: 2000});
 coffee.start();
 code.start();
 bugs.start();
@@ -32,31 +30,9 @@ var options = {
 
 
 var controller = new ScrollMagic.Controller();
+var portController = new ScrollMagic.Controller();
 
 $(document).ready(function(){
-  var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 400}).setPin("#pin").addTo(controller);
+  var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 400}).setPin("#pin1").addTo(controller);
+  var portfolioScene = new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 800}).setPin("#pin2").addTo(portController);
 });
-
-$(function () { // wait for document ready
-    // init
-    var controller2 = new ScrollMagic.Controller({
-      globalSceneOptions: {
-        triggerHook: 'onLeave'
-      }
-    });
-
-    // get all slides
-    var slides = document.querySelectorAll("section.panel");
-    // create scene for every slide
-    for (var i=0; i<slides.length; i++) {
-      if(slides[i] == 4){
-        new ScrollMagic.Scene({
-          triggerElement: slides[i]
-        }).setPin('.slide').addTo(controller2);
-      } else{
-          new ScrollMagic.Scene({
-            triggerElement: slides[i]
-          }).setPin(slides[i]).addTo(controller2);
-        }
-    }
-  });
