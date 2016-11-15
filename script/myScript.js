d7a7ec2fa216ee4c91532d1517ec6306015c54dc
Ben <nathb3210@gmail.com>
2016-11-14 21:20:50 -0500

    fix opacity scroll function

--- script/myScript.js
+++ script/myScript.js
@@ -2,6 +2,23 @@ $(".menu-collapsed").click(function() {
   $(this).toggleClass("menu-expanded");
 });
 
+var target = $('div.slide');
+var targetHeight = target.outerHeight();
+var containerHeight = $('.portfolio').outerHeight();
+
+var maxScroll = containerHeight - targetHeight;
+var scrollRange = maxScroll/(target.length-1);
+
+$(document).scroll(function(e){
+  var scrollY = $(window).scrollTop();
+  var scrollPercent = (scrollRange - scrollY%scrollRange)/scrollRange;
+  var divIndex = Math.floor(scrollY/scrollRange);
+
+  target.has(':lt(' + divIndex + ')').css('opacity', 0);
+  target.eq(divIndex).css('opacity', scrollPercent);
+  target.has(':gt(' + divIndex + ')').css('opacity', 1);
+});
+
 var coffee = new CountUp("coffeeCups", 0, 1528, 0, 5, options);
 var code = new CountUp("codeLines", 0, 100000, 0, 5, options);
 var bugs = new CountUp("bugsFixed", 0, 130, 0, 5, options);
